f2b9445d1cab1a1256f3714dd780a2ac8ca528e5
이우영 <wylee@qoo10.com>
2024-02-27 16:13:50 +0900

    상품 페이지 레이아웃 구현

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -1,7 +1,41 @@
+'use client'
+import styled from '@emotion/styled';
+import Header from '@/app/common/Header';
 
 interface Props {
   params: { [key:string]: string; }
 }
 export default function ProductMain({ params }: Props) {
-  return <div>Post:{ params.productId }</div>;
+  return (
+    <Container>
+      <Header />
+      <div className="content">
+        <div className="header">
+          <div>
+            <img src="https://picsum.photos/500/500" alt=""/>
+          </div>
+          <div>
+            상품 타이틀
+          </div>
+        </div>
+        <div>
+          상품 상세
+        </div>
+      </div>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 1084px;
+    margin: 0 auto;
+    .content {
+        .header {
+            display: flex;
+            flex-direction: row;
+        }
+    }
+
+`;
