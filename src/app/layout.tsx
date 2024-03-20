0c127e85cb37c00eedbcde5720981d80d7349978
wynter <u0lee.dev@gmail.com>
2024-03-20 18:12:57 +0900

    login page ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,5 +12,5 @@ export default function RootLayout({
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,3 +1,55 @@
+'use client';
+import styled from '@emotion/styled';
+
 export default function User() {
-  return <>user</>;
+  return (
+    <Container>
+      <div className="input-area">
+        <input type="text" placeholder="Email"/>
+      </div>
+      <div className="input-area">
+        <input type="text" placeholder="password"/>
+      </div>
+      <div className="login-btn">
+        <button>Sign in</button>
+      </div>
+      <div>
+        You don't have an account? <a href="/user/sign" className="signup">sign up</a>
+      </div>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+    height: 90vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    .input-area {
+        margin-bottom: 10px;
+
+        input {
+            line-height: 30px;
+            width: 300px;
+        }
+    }
+
+    .login-btn {
+        button {
+            margin-bottom: 10px;
+            line-height: 30px;
+            width: 310px;
+            background-color: #ff9e30;
+            border: #ff9e30 1px solid;
+            border-radius: 2px;
+            color: white;
+        }
+    }
+    
+    .signup {
+        color: #ff9e30;
+    }
+
+`;
